ccdd96ef6e95ecd5945e090166a9c3f35ac75753
jehanshah8 <jehan.shah8@gmail.com>
2022-04-21 02:06:32 -0400

    as close to pseudo code as possible

--- src/pca_p2p_node.py
+++ src/pca_p2p_node.py
@@ -1,6 +1,7 @@
 import imp
 import p2p_node
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 class PCANode(p2p_node.Node):
@@ -8,10 +9,8 @@ class PCANode(p2p_node.Node):
         super().__init__(hostname, port, id, max_connections, debug)
 
         self.n_components = t
-        self.df = data
+        self.df = data.to_numpy()
         self.mean = None
-        
-
 
         self.singular_values_recv = {}
         self.singular_vectors_recv = {}
@@ -20,23 +19,38 @@ class PCANode(p2p_node.Node):
 
     
     def round_one(self):
+        self.debug_print(f'Node.round1: ')
         self.mean = np.mean(self.df, axis=0)
         self.df -= self.mean
-        self.left_singular_vectors, self.singular_values, self.right_singular_vectors = np.linalg.svd(self.df, full_matrices=False)
-        self.left_singular_vectors, self.right_singular_vectors = np.linalg.svd_flip(self.left_singular_vectors, self.right_singular_vectors)
+        self.U, self.S, V_T = np.linalg.svd(self.df, full_matrices=False)
+        self.U, V_T = np.linalg.svd_flip(self.U, V_T)
 
-        self.singular_values = self.singular_values[:self.n_components]
-        self.right_singular_vectors = np.transpose(self.right_singular_vectors[:self.n_components])
-        self.debug_print(f'Node.round1: ')
-
+        self.S = self.S[:self.n_components]
+        self.V = np.transpose(V_T[:self.n_components])
 
         msg = {}
-        msg['singular_values'] = np.ndarray.tolist(self.singular_values)
-        msg['singular_vectors'] = np.ndarray.tolist(self.right_singular_vectors)
+        #msg['singular_values'] = np.ndarray.tolist(self.S)
+        msg['singular_vectors'] = np.ndarray.tolist(self.V)
         self.broadcast(msg)
 
     def round_two(self):
-        pass
+        self.g_cov_mat = np.zeros((self.n_components, self.n_components))
+    
+        for id, V_t in self.singular_vectors_recv.items():
+            P_i_t = np.matmul(self.df, V_t) 
+            cov_mat = np.matmul(np.transpose(P_i_t), P_i_t)
+            self.g_cov_mat = np.add(self.g_cov_mat, cov_mat) 
+
+        eigen_values, eigen_vectors = np.linalg.eig(self.g_cov_mat)
+        eigen_stuff = list(zip(eigen_values, eigen_vectors))
+        eigen_stuff = sorted(eigen_stuff, key=lambda x: x[0], reverse=True)
+        sorted_eigen_values, sorted_eigen_vectors = zip(*eigen_stuff)
+        sorted_eigen_vectors = np.array(sorted_eigen_vectors)
+        sorted_eigen_vectors = sorted_eigen_vectors[:, :self.n_components]
+        self.P_i_hat = np.matmul(P_i_t, sorted_eigen_vectors)
+        P_i_hat = np.matmul(P_i_hat, np.transpose(sorted_eigen_vectors))
+
+
 
     def message_handler(self, sender_id, msg):
         super().message_handler(sender_id, msg)    
@@ -52,7 +66,11 @@ class PCANode(p2p_node.Node):
                 else:
                     self.debug_print(f'Node.message_handler: Unknown key {key}')
 
-        # TODO: add more robust compare? 
-        if len(self.P_i_hats_recv) >= len(self.connections):
-            #self.P_hat = 
-            pass
+        # TODO: add more robust comparisions? 
+        if len(self.singular_vectors_recv) >= len(self.connections): #got V from all nodes
+            self.round_two()
+        
+        if len(self.P_i_hats_recv) >= len(self.connections): 
+            self.P_hat = np.concatenate(self.P_i_hats_recv.values(), axis=0)
+
+    
--- src/playground.py
+++ src/playground.py
@@ -1,6 +1,3 @@
-
-import dis
-from math import dist
 import sys
 import pandas as pd
 
@@ -11,6 +8,18 @@ from sklearn.decomposition import PCA
 from sklearn.utils.extmath import svd_flip
 from matplotlib import pyplot as plt
 import os
+
+def plot(projected_data, labels, figname):
+    '''
+    Input:
+        lowDDataMat: the 2-d data after PCA transformation obtained from pca function
+        labelMat: the corresponding label of each observation obtained from loadData
+    '''
+    print(np.shape(projected_data))
+    projected_data = np.transpose(projected_data)
+    #print(lowDDataMatT)
+    plt.scatter(projected_data[0],projected_data[1], c=labels)
+    plt.savefig(figname)
 
 def load_dataset(dataset_path='iris_with_cluster.csv', seperate_feats=True):
     df = pd.read_csv(dataset_path)
@@ -46,32 +55,89 @@ def split_dataset(dataset_path, n_chunks, write=True):
 def round_one(P_i, n_components):
         mean = np.mean(P_i, axis=0)
         P_i -= mean
-        U, D, E_T = linalg.svd(P_i, full_matrices=False)
+        #print(f'data shape = {np.shape(local_datasets[i])}')
+        U_i, D_i, E_i_T = linalg.svd(P_i)
+        #print(f'U shape = {np.shape(U_i)}')
+        #print(f'D shape = {np.shape(D_i)}')
+        #print(f'E_T shape = {np.shape(E_i_T)}')
         # flip eigenvectors' sign to enforce deterministic output
-        U, E_T = svd_flip(U, E_T)
-        #print(D)
-        #print(D[:n_components])
-    #
-        #print()
-    #
-        #print(np.transpose(E_T))
-        #print(np.transpose(E_T[:n_components]))
-        return D[:n_components], np.transpose(E_T[:n_components])
+        #u, e_T = svd_flip(u, e_T)
+        
+        D_i_t = np.zeros_like(P_i)
+        for i in range(n_components):
+            D_i_t[i][i] = D_i[i]
+        #print(f'D_t shape = {np.shape(D_i_t)}')
+        #print(D_i)
+        #print(D_i_t)
 
-def round_two(singular_values_recv, singular_vectors_recv, P_i, n_components, id):
-    g_cov_mat = np.zeros((n_components, n_components))
-    P_i = P_i.to_numpy()
+        # algo way to get P_i_t
+        P_i_t = matmul(U_i, D_i_t)
+        P_i_t = matmul(P_i_t, E_i_T)
+        #
+        
+        # my way
+        #P_i_t = matmul(P_i, np.transpose(E_i_T[:n_components]))
+        #
+
+        #print(f'P_i_t {P_i_t}')
+        #print(f'data shape after t = {np.shape(local_datasets[i])}')
+
+        return D_i[:n_components], np.transpose(E_i_T[:n_components]), P_i_t
+
+def round_two(singular_values_recv, singular_vectors_recv, P_i_t, n_components):
+    #P_i_t = P_i_t.to_numpy()
+    #print(f'shape of local data {np.shape(P_i_t)}')
+
+    g_cov_mat = np.zeros((np.shape(P_i_t)[1], np.shape(P_i_t)[1]))  #either txt or dxd? 
+    
+    #print(f'shape of g_cov_mat {np.shape(g_cov_mat)}')
+
+    #for data from each node
     for i in range(len(singular_values_recv)):
+        
+        D_i_t = np.zeros((np.shape(P_i_t)[0], n_components))
+        for j in range(n_components):
+            D_i_t[j][j] = singular_values_recv[i][j]
+
+        #print(f'D_t shape = {np.shape(d_i_t)}')
+
+        E_i_t = singular_vectors_recv[i] 
+        #print(f'E_t shape = {np.shape(e_i_t)}')
+        
+
+        #One way to get cov mats
+        
+        cov_mat = np.matmul(E_i_t, np.transpose(D_i_t))
+        cov_mat = np.matmul(cov_mat, D_i_t)
+        cov_mat = np.matmul(cov_mat, np.transpose(E_i_t))
+        
+        #second way to get cov mats
+        """
         D_t = np.diag(singular_values_recv[i])
         #print(f'D_t shape = {np.shape(D_t)}')
 
         E_t = singular_vectors_recv[i]
         #print(f'E_t shape = {np.shape(E_t)}')
-
-        P_i_t = np.matmul(P_i, E_t) 
+                            #p = n x d
+        
+        P_i_t = np.matmul(P, E_t) #E_t = d x t # projection of 
+        # local data on t components using its own singular vectors
+        
         cov_mat = np.matmul(np.transpose(P_i_t), P_i_t)
         g_cov_mat = np.add(g_cov_mat, cov_mat) 
-    
+        # E = d x d -> first t columns 
+        #E_t * (D_t)transpose = d x t * t x n
+        #(E_t * (D_t)transpose) * D_t = d x n * n x t = d x t 
+        #d x t * t x d = d x d 
+        #should be t x t 
+        """
+
+
+        g_cov_mat = np.add(g_cov_mat, cov_mat) 
+
+
+    # CERTAIN ABOUT BELOW 
+
     eigen_values, eigen_vectors = np.linalg.eig(g_cov_mat)
     #print(f'eigen_values: {eigen_values}')
     #print(f'eigen_vectors: {eigen_vectors}')
@@ -86,47 +152,42 @@ def round_two(singular_values_recv, singular_vectors_recv, P_i, n_components, id
 
     sorted_eigen_vectors = sorted_eigen_vectors[:, :n_components]
     #print(f't eigen_vectors: {sorted_eigen_vectors}')
-    P_i_t = np.matmul(P_i, singular_vectors_recv[id]) 
     P_i_hat = np.matmul(P_i_t, sorted_eigen_vectors)
-    P_i_hat = np.matmul(P_i_hat, np.transpose(sorted_eigen_vectors))
+    #P_i_hat = np.matmul(P_i_hat, np.transpose(sorted_eigen_vectors)) ## adding this means no dim reduction on transform
     return P_i_hat
 
         
     #np.concatenate(cov_mats, axis=0)
     #print(g_cov_mat)
 
-def plot(projected_data, labels, figname):
-    '''
-    Input:
-        lowDDataMat: the 2-d data after PCA transformation obtained from pca function
-        labelMat: the corresponding label of each observation obtained from loadData
-    '''
-
-    projected_data = np.transpose(projected_data)
-    #print(lowDDataMatT)
-    plt.scatter(projected_data[0],projected_data[1], c=labels)
-    plt.savefig(figname)
-
 if __name__ == "__main__":
     
     n_nodes = 5
-    #dataset_path = '/datasets/cho/cho.csv'
-    dataset_path = '/datasets/iris/iris_with_cluster.csv'
+    dataset_path = '/datasets/cho/cho.csv'
+    #dataset_path = '/datasets/iris/iris_with_cluster.csv'
     dataset_path = os.getcwd() + dataset_path
     n_components = 2
 
-    ### central
+    
+    
     data_mat, label_mat = load_dataset(dataset_path)
     label_mat = np.array(label_mat)
-    pca = PCA(n_components)  # project from 64 to 2 dimensions
-    projected_2 = pca.fit_transform(data_mat)
-    print(np.shape(projected_2))
-    plot(projected_2, label_mat, 'central')
+
+    ### og data
+    #plot(data_mat., label_mat, 'og-data')
+
+    #print(np.shape(data_mat))
     
+    ### central
+    pca = PCA(n_components) 
+    projected_2 = pca.fit_transform(data_mat)
+    #print(np.shape(projected_2))
+    plot(projected_2, label_mat, 'central')
+
+
 
 
     local_data, local_labels = split_dataset(dataset_path, n_nodes, write=False)
-
 
     """
     local_data = []
@@ -139,22 +200,27 @@ if __name__ == "__main__":
 
     singular_values_recv = []
     singular_vectors_recv = []
-
-    for d in local_data:
-        D_t, E_t = round_one(d, n_components)
+    P_i_t_recv = []
+    for i in range(n_nodes):
+        D_t, E_t, P_t = round_one(local_data[i], n_components)
         singular_values_recv.append(D_t)
         singular_vectors_recv.append(E_t)
+        P_i_t_recv.append(P_t)
+
+    print('end round 1')
 
     P_i_hats_recv = []
     for i in range(n_nodes):
-        P_i_hats_recv.append(round_two(singular_values_recv, singular_vectors_recv, local_data[i], n_components, i))
-    
+        P_hat = round_two(singular_values_recv, singular_vectors_recv, P_i_t_recv[i], n_components)
+        P_i_hats_recv.append(P_hat)
+    print('end round 2')
+
     projected_1 = np.concatenate(P_i_hats_recv, axis=0)
     dist_label_mat = np.concatenate(local_labels, axis=0)
     dist_label_mat = np.array(dist_label_mat)
 
     plot(projected_1, dist_label_mat, 'distributed')
-    print(np.shape(projected_1))
+    #print(np.shape(projected_1))
     #print()
     #print()
     #print()
