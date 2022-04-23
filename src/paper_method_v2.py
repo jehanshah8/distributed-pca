caa7c1956493fc91c7333c6c841f31f8e919361b
Sai Mudumba <56524843+smudumba2019@users.noreply.github.com>
2022-04-23 18:20:10 -0400

    Add files via upload

--- src/paper_method_v2.py
+++ src/paper_method_v2.py
@@ -120,10 +120,11 @@ def round_two(singular_values_recv, singular_vectors_recv, P_i_t, n_components):
     #print(g_cov_mat)
     # CERTAIN ABOUT BELOW
     U, D, E_T = linalg.svd(g_cov_mat, full_matrices=True)
-   
+    
     P_i_hat = np.matmul(P_i_t, np.transpose(E_T[:n_components]))
     # adding below means no dim reduction on transform
-    #P_i_hat = np.matmul(P_i_hat, E_T[:n_components])
+    P_i_hat = np.matmul(P_i_hat, E_T[:n_components])
+    print(P_i_hat.shape)
     return P_i_hat, g_cov_mat
 
 def TestEvaluationMetric2(P_t, P_t_hat, n_components):
@@ -139,50 +140,40 @@ def TestEvaluationMetric2(P_t, P_t_hat, n_components):
         for j in range(0,P_t.shape[0]): # for each sample 
             sum_max += max( P_t[j,i], P_t_hat[j,i] )
             sum_min += min( P_t[j,i], P_t_hat[j,i] )
-            print(sum_max, sum_min)
+            # print(sum_max, sum_min)
         # print(P_t, P_t_hat)
-        print(sum_min, sum_max)
+        # print(sum_min, sum_max)
         Jaccard_Distance.append( (1 - abs(sum_min/sum_max) ) )
     return Jaccard_Distance
 
-def TestEvaluationMetric(P_t, P_t_hat, n_components):
-    """ Jaccard Distance:
-        P_t : (n x t)
-        P_t_hat : (n x t)
-    """
-    Jaccard_Distance = []
+def TestEvaluationMetric(X, P_t_hat, n_components):
+    subtraction = ((X)**2 - (P_t_hat)**2)
     
-    for j in range(0,P_t.shape[0]): # for each reduced dimensions
-        sum_max = 0
-        for i in range(0,n_components): # for each sample 
-            sum_max += (abs(P_t[j,i]) - abs(P_t_hat[j,i]))**2
-            
-        Jaccard_Distance.append( sum_max**0.5 )
-
-    return sum(Jaccard_Distance)
-    
-def TestEvaluationMetric3(Cov_Central, Cov_Dist, n_components):
-    Uc, Dc, E_Tc = linalg.svd(Cov_Central, full_matrices=True)
-    Ud, Dd, E_Td = linalg.svd(Cov_Dist, full_matrices=True)
-    print(Dc)
-    print(Dd)
-    Percent_Exp_Variance_Central = sum(Dc[0:n_components])/sum(Dc) * 100
-    Percent_Exp_Variance_Dist = sum(Dd[0:n_components])/sum(Dc) * 100 # divide by sum of Dc not Dd
-    print(Percent_Exp_Variance_Central, Percent_Exp_Variance_Dist)
-    plt.bar(range(len(Dc)),Dc)
-    plt.show()
-    plt.bar(range(len(Dd)),Dd)
-    plt.show()
-    return
+def TestEvaluationMetric3(C, D, n_components):
+    Uc, Dc, E_Tc = linalg.svd(C, full_matrices=True)
+    Ud, Dd, E_Td = linalg.svd(D, full_matrices=True)
+    # print(Dc)
+    # print(Dd)
+    Dc = (Dc**2) / (C.shape[0] - 1)
+    Dd = (Dd**2) / (D.shape[0] - 1)
+    Percent_Exp_Variance_Central = sum(Dc[0:n_components])#/sum(Dc) * 100
+    Percent_Exp_Variance_Dist = sum(Dd[0:n_components])#/sum(Dc) * 100 # divide by sum of Dc not Dd
+    # print(Percent_Exp_Variance_Dist/Percent_Exp_Variance_Central)
+    # plt.bar(range(len(Dc)),Dc)
+    # plt.show()
+    # plt.bar(range(len(Dd)),Dd)
+    # plt.show()
+    return Percent_Exp_Variance_Dist#/Percent_Exp_Variance_Central
     
 
 if __name__ == "__main__":
 
-    n_nodes = 2
+    
+    n_nodes_all = 20
     dataset_path = 'C:/Users/saimu/OneDrive - purdue.edu/Purdue Graduate School/MS_Electrical_Computer_Engineering/Second Year 2020-2021/ECE 60872 Fault Tolerance Design/Project/distributed-pca-p2p-network/distributed-pca-p2p-network/datasets/cho/cho.csv'
     # dataset_path = 'C:/Users/saimu/OneDrive - purdue.edu/Purdue Graduate School/MS_Electrical_Computer_Engineering/Second Year 2020-2021/ECE 60872 Fault Tolerance Design/Project/distributed-pca-p2p-network/distributed-pca-p2p-network/datasets/iris/iris_with_cluster.csv'
     # dataset_path = os.getcwd() + dataset_path
-    n_components = 4
+    n_components_all = 16
 
     data_mat, label_mat = load_dataset(dataset_path)
     label_mat = np.array(label_mat)
@@ -194,50 +185,80 @@ if __name__ == "__main__":
     
     #cov_mat = np.cov(np.transpose(data_mat))
     #print(cov_mat)
+
+    ratio_dict = {}
+    for n_nodes in range(1, n_nodes_all):
+        list_temp = []
+        for n_components in range(2, n_components_all):
+            local_data, local_labels = split_dataset(
+                dataset_path, n_nodes, write=False)
+        
+            singular_values_recv = []
+            singular_vectors_recv = []
+            P_i_t_recv = []
+            for i in range(n_nodes):
+                D_t, E_t, P_t = round_one(local_data[i], n_components)
+                singular_values_recv.append(D_t)
+                singular_vectors_recv.append(E_t)
+                P_i_t_recv.append(P_t)
+            # print('end round 1')
+        
+            P_i_hats_recv = []
+            for i in range(n_nodes):
+                P_hat, covMat_dist = round_two(singular_values_recv,
+                                  singular_vectors_recv, P_i_t_recv[i], n_components)
+                P_i_hats_recv.append(P_hat)
+            # print('end round 2')
+        
+            projected_1 = np.concatenate(P_i_hats_recv, axis=0)
+            
+            # identity = np.array([[-1,0],[0,-1]])
+            # identity = -1*np.identity(n_components)
+            # projected_1 = np.matmul(projected_1,identity)
+            
+            dist_label_mat = np.concatenate(local_labels, axis=0)
+            dist_label_mat = np.array(dist_label_mat)
+        
+            # plot(projected_1, dist_label_mat, 'distributed')
+            # print('end test')
+            X = data_mat
+            X = X - np.mean(X, axis=0)
+            X = np.array(X)
+            # JD = TestEvaluationMetric2(projected_1, X, n_components)
+            # print(JD)
+            # plot(X, label_mat, 'dataset')    
+            
+            covMat = np.cov(np.transpose(X))
+            ratio = TestEvaluationMetric3(X, projected_1, n_components)
+            list_temp.append(ratio)
+        ratio_dict[n_nodes] = list_temp
     
     ### central
     pca = PCA(n_components)
     projected_2 = pca.fit_transform(data_mat)
     # print(np.shape(projected_2))
     plot(projected_2, label_mat, 'central')
-
-    local_data, local_labels = split_dataset(
-        dataset_path, n_nodes, write=False)
-
-    singular_values_recv = []
-    singular_vectors_recv = []
-    P_i_t_recv = []
-    for i in range(n_nodes):
-        D_t, E_t, P_t = round_one(local_data[i], n_components)
-        singular_values_recv.append(D_t)
-        singular_vectors_recv.append(E_t)
-        P_i_t_recv.append(P_t)
-    print('end round 1')
-
-    P_i_hats_recv = []
-    for i in range(n_nodes):
-        P_hat, covMat_dist = round_two(singular_values_recv,
-                          singular_vectors_recv, P_i_t_recv[i], n_components)
-        P_i_hats_recv.append(P_hat)
-    print('end round 2')
-
-    projected_1 = np.concatenate(P_i_hats_recv, axis=0)
     
-    # identity = np.array([[-1,0],[0,-1]])
-    # identity = -1*np.identity(n_components)
-    # projected_1 = np.matmul(projected_1,identity)
+    # JD = TestEvaluationMetric2(projected_2, projected_1, n_components)
+    # print(JD)
     
-    dist_label_mat = np.concatenate(local_labels, axis=0)
-    dist_label_mat = np.array(dist_label_mat)
-
-    plot(projected_1, dist_label_mat, 'distributed')
-    print('end test')
-    
-    JD = TestEvaluationMetric(projected_2, projected_1, n_components)
-    print(JD)
     
     X = data_mat
     X = X - np.mean(X, axis=0)
     X = np.array(X)
+    JD = TestEvaluationMetric2(projected_1, X, n_components)
+    # print(JD)
+    plot(X, label_mat, 'dataset')    
+    
     covMat = np.cov(np.transpose(X))
     TestEvaluationMetric3(covMat, covMat_dist, n_components)
+    
+    #################################################################
+    """
+    PLOTTING
+    """
+    for v in range(1, n_nodes_all):
+        plt.plot(range(2, n_components_all), ratio_dict[v], label=v)
+    plt.legend()
+    plt.show()
+    
